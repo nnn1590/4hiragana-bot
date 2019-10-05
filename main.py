d3b4abf920da8d3b365300cab61ec94545682eea
nnn1590 <28985763+nnn1590@users.noreply.github.com>
2019-10-05 18:46:36 +0900

    Add options and Python 2 support

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
+#!/usr/bin/python
 # coding: utf-8
 
-#thanks
-#原案：twitter:3hiragana_bot様
-#ひらがなのランダム生成:https://qiita.com/sora410/items/aad7e17c8c9c35a94f45 qiita:@sora410様
+# thanks
+# 原案：twitter:3hiragana_bot様
+# ひらがなのランダム生成:https://qiita.com/sora410/items/aad7e17c8c9c35a94f45 qiita:@sora410様
+# Pythonistaなら知ってるオプションパーサ:https://qiita.com/petitviolet/items/aad73a24f41315f78ee4#sysargv%E3%82%92%E4%BD%BF%E3%81%86 qiita:@petitviolet様
 ###
+from __future__ import unicode_literals
 import random
-import tweepy
+import sys
 
 CK = "insert key here"  # Consumer API key
 CS = "insert secret key here"  # Consumer API secret key
 AT = "insert token here"  # Access token
 AS = "insert secret token here"  # Access token secret
-
-auth = tweepy.OAuthHandler(CK, CS)
-auth.set_access_token(AT, AS)
-
-api = tweepy.API(auth)
-
+tweeting = True
+length = 4
 text = ""
-for i in range(4):
-    text += chr(random.randint(ord('あ'),ord('ん')))
+usage = 'Usage: python3 {} FILE [-n|--no-tweet] [-l|--length <number>] [-h|--help]'.format(__file__)
+
+if 1 < len(sys.argv):
+    arguments = sys.argv
+    arguments.pop(0)
+    fname = arguments[0]
+    options = [option for option in arguments if option.startswith('-')]
+    if '-h' in options or '--help' in options:
+        print(usage)
+        exit()
+    if '-n' in options or '--no-tweet' in options:
+        tweeting = False
+    if '-l' in options or '--length' in options:
+        length_position = arguments.index('-l') \
+            if '-l' in options else arguments.index('--length')
+        length = int(arguments[length_position + 1])
+
+for i in range(length):
+    if sys.version_info[0] < 3:
+        text += unichr(random.randint(ord('あ'), ord('ん')))
+    else:
+        text += chr(random.randint(ord('あ'), ord('ん')))
 print(text)
-api.update_status(text)
+
+if tweeting == True:
+    import tweepy
+
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+
+    api = tweepy.API(auth)
+
+    api.update_status(text)
